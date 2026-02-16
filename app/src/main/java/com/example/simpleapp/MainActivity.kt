82d6eb844760eacf4afb863a7f506fd23849764e
hearis71 <97028433+hearis71@users.noreply.github.com>
2026-02-17 03:28:33 +0700

    butt

--- app/src/main/java/com/example/simpleapp/MainActivity.kt
+++ app/src/main/java/com/example/simpleapp/MainActivity.kt
@@ -2,10 +2,23 @@ package com.example.simpleapp
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import android.widget.TextView
+import com.google.android.material.button.MaterialButton
 
 class MainActivity : AppCompatActivity() {
+
+    private var counter = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val txtCounter = findViewById<TextView>(R.id.txtCounter)
+        val btnAdd = findViewById<MaterialButton>(R.id.btnAdd)
+
+        btnAdd.setOnClickListener {
+            counter++
+            txtCounter.text = counter.toString()
+        }
     }
 }
